e3867b7f58c0fa667ebcb1eb3757b5a8cab47680
graciekilner <gk05264525@priestley.ac.uk>
2024-12-13 15:55:43 +0000

    i dont know

---
+++ Income-Management/__init__.py
@@ -0,0 +1,49 @@
+class IncomeManagement:
+    def __init__(self,sourse,amount):
+        self.sourse=print(input("input a sourse"))
+        self.amount=print(input("input an amount"))
+
+def add_income(sourse, amount,self):
+    pass
+
+def get_total_income(self):
+    pass
+
+def main():
+ 
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
