0bb53aacb99ed475827689364e7e5a059c2c03d4
Jack07-08 <jg05263044@priestley.ac.uk>
2024-12-13 15:20:43 +0000

    def main functions (amount, category)

--- Expense-Tracking/__init__.py
+++ Expense-Tracking/__init__.py
@@ -1,8 +1,17 @@
 def add_expense(category, amount):
-    pass
-
+    print(category, amount)
 def get_total_expenses():
     pass
 
 def get_expenses_by_category(category):
     pass
+
+def main():
+    amount = input("userr money value \n")
+    category = input("What category are you using? \n")
+    try:
+        add_expense(category, amount)
+    except:
+        print("Error")
+
+main()
