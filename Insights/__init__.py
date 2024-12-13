f07af230cb80a7f3f848412e34e55943cee731d0
sions5801 <ss05264491@priestley.ac.uk>
2024-12-13 15:44:30 +0000

    add a basic message telling the user their income, expenses, budget and savings. it also informs them of how much they went over their budget if that is the case

--- Insights/__init__.py
+++ Insights/__init__.py
@@ -5,5 +5,8 @@ def calculate_savings(total_income, total_expenses):
     return savings
     
 
-def generate_report():
-    pass
+def generate_report(total_income, total_expenses, savings, budget):
+    if budget >= total_expenses:
+        print(f"You made £{total_income} and spent £{total_expenses} out of your budget of £{budget}. Overall you saved £{savings}.")
+    else:
+        print(f"You made £{total_income} and spent £{total_expenses}. You spent £{total_expenses-budget} more than your budget of £{budget}.")
