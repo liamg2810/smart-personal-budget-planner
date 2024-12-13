de99619aacab5929b797982e3b0da0c1163822d4
sions5801 <ss05264491@priestley.ac.uk>
2024-12-13 15:47:06 +0000

    change report from being print statement to having it returned from the function

--- Insights/__init__.py
+++ Insights/__init__.py
@@ -7,6 +7,6 @@ def calculate_savings(total_income, total_expenses):
 
 def generate_report(total_income, total_expenses, savings, budget):
     if budget >= total_expenses:
-        print(f"You made £{total_income} and spent £{total_expenses} out of your budget of £{budget}. Overall you saved £{savings}.")
+        return(f"You made £{total_income} and spent £{total_expenses} out of your budget of £{budget}. Overall you saved £{savings}.")
     else:
-        print(f"You made £{total_income} and spent £{total_expenses}. You spent £{total_expenses-budget} more than your budget of £{budget}.")
+        return(f"You made £{total_income} and spent £{total_expenses}. You spent £{total_expenses-budget} more than your budget of £{budget}.")
