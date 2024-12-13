aa0de6a1cf5ff0b34917bc78a76b62e70bd64a8c
lg05264093 <lg05264093@LC21205XX>
2024-12-13 15:25:55 +0000

    Added matplotlib import and download

--- Reporting/__init__.py
+++ Reporting/__init__.py
@@ -1,3 +1,17 @@
+import subprocess
+
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    subprocess.check_call(["python", "-m", "pip", "install", "matplotlib"])
+
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        print("Error installing matplotlib. Please install it manually.")
+        exit(1)
+
+
 def plot_income_vs_expenses():
     pass
 
