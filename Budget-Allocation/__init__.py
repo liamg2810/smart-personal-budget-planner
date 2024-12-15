6f967d778a7f0272c8dc7a538ea5c2281dd1acc0
Supern_Ice <alfietompsett@outlook.com>
2024-12-15 19:04:04 +0000

    Mapped out the main functions, need to add file handling

--- Budget-Allocation/__init__.py
+++ Budget-Allocation/__init__.py
@@ -1,13 +1,38 @@
 def main():
-    inpCategory = input("What is your category? \n")
-    inpAmount = int(input("What is your amount? \n"))
-    check_budget(inpCategory, inpAmount)
+    select = selection()
+    selected = False
+    while selected==False:
+        if select==1:
+            add_category()
+            selected=True
+        elif select==2:
+            append_category()
+            selected=True
+        elif select==3:
+            check_budget()
+            selected=True
+        else:
+            print("Please enter a valid selection")
+            select = selection()
 
-def set_budget(category, limit):
-    pass
 
-def check_budget(category, amount):
-    print(category, amount)
+
+
+def selection(): 
+    select=int(input("Would you like to: \n1.Add a new category and budget \n2.Append an existing categories budget \n3.Check a cost against a characters budget"))
+    return select
+
+def add_category():
+    category = input("Enter the category you would like to add: ")
+    budget = int(input("Enter the budget for this category: "))
+
+def append_category():
+    category = input("Enter the category you would like to append: ")
+    budget = int(input("Enter the amount you would like to append: "))
+
+def check_budget():
+    category = input("Enter the category you would like to check: ")
+    cost = int(input("Enter the cost of the item: "))
 
 if __name__ == "__main__":
     print("Unit testing now!")
