a8c8d70ec2bd39d0be44d2111e8c1f3e65701475
graciekilner <gk05264525@priestley.ac.uk>
2024-12-16 10:29:05 +0000

    Start to map out functions and store income within a list

--- Income-Management/__init__.py
+++ Income-Management/__init__.py
@@ -1,18 +1,23 @@
-class IncomeManagement:
-    def __init__(self,sourse,amount):
-        self.sourse=print(input("input a sourse"))
-        self.amount=print(input("input an amount"))
+income_list=[]
 
-def add_income(sourse, amount,self):
+def add_income(source,amount):
+    source=(input("source of income:"))
+    amount=float(input("amount:"))
+    
+    income_dictionary={'source',source,'amount',amount}
+    income_list.append(income_dictionary)
+
+
+def get_total_income():
     pass
 
-def get_total_income(self):
-    pass
+
 
 def main():
- 
-    main()
+    pass
 
+if __name__ == "__main__":
+    main()
 
 
 
