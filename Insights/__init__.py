91165990087d648d521b5dce29b3926203979b6d
sions5801 <ss05264491@priestley.ac.uk>
2024-12-18 14:07:03 +0000

    finish up generate report module

--- Insights/__init__.py
+++ Insights/__init__.py
@@ -5,7 +5,7 @@ def calculate_savings(total_income, total_expenses): #calculates the total savin
     return savings
 
 def find_highest(sources):
-    highest = None
+    highest = 0
     highest_source = None
     for source, value in sources.items():
         if value > highest:
@@ -13,16 +13,18 @@ def find_highest(sources):
             highest_source = source
     return highest, highest_source
     
-def generate_report(total_income, total_expenses, budget, income_entries, expense_entries): #generates a report.
+def generate_report(total_income, total_expenses, over_budget, over_budget_difference, income_entries, expense_entries): #generates a report.
     
     highest_expense, highest_expense_source = find_highest(expense_entries)
+    highest_income, highest_income_source = find_highest(income_entries)
     savings = calculate_savings(total_income, total_expenses)
     
-    if budget >= total_expenses: #check if you went over your budget; the text will change if you did.
+    if over_budget == True: #check if you went over your budget; the text will change if you did
         #return(f"You made £{total_income} and spent £{total_expenses} out of your budget of £{budget}. Overall you saved £{savings}.")
-        return(f"")
+        return(f"You made £{total_income} in total. Your highest source of income was {highest_income_source} which made you £{highest_income}. Overall you spent £{total_expenses} with the source you spend the most money on being {highest_expense_source}, which you spent £{highest_expense} on. You saved £{savings} in total. You went over your budget by £{over_budget_difference}.")
     else:
-        return(f"You made £{total_income} and spent £{total_expenses}. You spent £{total_expenses-budget} more than your budget of £{budget}.")
+        return(f"You made £{total_income} in total. Your highest source of income was {highest_income_source} which made you £{highest_income}. Overall you spent £{total_expenses} with the source you spend the most money on being {highest_expense_source}, which you spent £{highest_expense} on. You saved £{savings} in total.")
+
 """
 expenselist = {
     "source1": 200,
@@ -31,7 +33,13 @@ expenselist = {
     "source4": 413,
     "source5": 43
 }
-"""
 
+incomelist = {
+    "source1": 200,
+    "source2": 300,
+    "source3": 10,
+    "source4": 413,
+    "source5": 43
+}
 
-#generate_report(1000,700, 900, "income", expenselist)
+print(generate_report(1000,700, True, 50, expenselist, incomelist))"""
