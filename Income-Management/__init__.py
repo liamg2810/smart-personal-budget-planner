bf743e3f67d53f2588115981a59e2314cd35007e
graciekilner <gk05264525@priestley.ac.uk>
2024-12-19 14:54:59 +0000

    Write and load to and from JSON

--- Income-Management/__init__.py
+++ Income-Management/__init__.py
@@ -1,9 +1,12 @@
-income_list=[]
+import json
 
-def add_income():
-    source=(input("source of income:"))
-    amount=float(input("amount:"))
-    
+
+with open('Income-Management/incomeManagement.json', 'r') as file:
+    income_list = json.load(file)
+
+
+
+def add_income(source,amount):
     income_dictionary={'source':source,'amount':amount}
     income_list.append(income_dictionary)
 
@@ -13,16 +16,15 @@ def get_total_income():
         print(income_dictionary["amount"])
         total=0
 
-
-
-
 def main():
+
+
     while True:
       print("\nincome managment menu:\n")
       print("1.add income")
       print("2.get total income")
       print("3.Exit") 
-      choice=input("enter whic option(1,2,3)")
+      choice=input("enter which option(1,2,3)")
 
       if choice == "1":
          source=(input("source of income:")) 
@@ -35,9 +37,12 @@ def main():
           break
       else:
           print("invalid option please try again and input options 1,2 or 3")
-          
-   
     
+  
+
+    with open("Income-Management/incomeManagement.json", mode="w", encoding="utf-8") as write_file:
+        print(income_list)
+        json.dump(income_list, write_file)
 
 if __name__ == "__main__":
     main()
